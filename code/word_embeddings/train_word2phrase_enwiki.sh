f52fdc27f2c09b9dd8dd3186af3ab6142cebecff
Jonas Triki <trikijonas@gmail.com>
2020-10-30 10:39:08 +0100

    Update word2phrase arguments

--- code/word_embeddings/train_word2phrase_enwiki.sh
+++ code/word_embeddings/train_word2phrase_enwiki.sh
@@ -1,11 +1,11 @@
 python train_word2phrase.py \
---text_data_filepath data/enwiki-20200901/enwiki-20200901-01.txt \
---dataset_name enwiki-20200901_testing \
+--text_data_dir data/enwiki-20200901 \
+--dataset_name enwiki-20200901 \
 --starting_epoch_nr 1 \
---n_epochs 4 \
+--n_epochs 2 \
 --max_vocab_size -1 \
 --min_word_count 5 \
 --threshold 200 \
---threshold_decay 0.75 \
+--threshold_decay 0.5 \
 --phrase_sep _ \
 --output_dir data
