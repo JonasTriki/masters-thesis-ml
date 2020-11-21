34e60a730ad8524cbc02db8aedf11e2f94622041
Jonas Triki <trikijonas@gmail.com>
2020-11-21 22:34:48 +0100

    Update script for downloading geodata

--- code/analysis_of_embeddings/preprocess_analysis_data.py
+++ code/analysis_of_embeddings/preprocess_analysis_data.py
@@ -1,15 +1,17 @@
 import argparse
+import re
 import sys
 import zipfile
 from os import makedirs
 from os.path import isdir, isfile, join
 
 import pandas as pd
+import requests
 
 sys.path.append("..")
 
 from text_preprocessing_utils import preprocess_text
-from utils import download_from_url
+from utils import download_from_url, get_cached_download_text_file
 
 
 def parse_args() -> argparse.Namespace:
@@ -34,15 +36,35 @@ def parse_args() -> argparse.Namespace:
         default="",
         help="Output directory to save processed data",
     )
+    parser.add_argument(
+        "--geonames_username",
+        type=str,
+        default="jtr008",
+        help="GeoNames username (create account here: https://www.geonames.org/login)",
+    )
     return parser.parse_args()
 
 
-def preprocess_country_capitals(raw_data_dir: str, output_dir: str) -> None:
+def preprocess_name(name: str) -> str:
+    """
+    TODO: Docs
+    """
+    remove_brackets_re = re.compile("^(.+?)[(\[].*?[)\]](.*?)$")
+    name_no_brackets_results = re.findall(remove_brackets_re, name)
+    if len(name_no_brackets_results) > 0:
+        name = "".join(name_no_brackets_results[0]).strip()
+    name = "_".join(preprocess_text(name.replace("'", "")))
+    return name
+
+
+def preprocess_country_info(
+    raw_data_dir: str, output_dir: str, geonames_username: str
+) -> None:
     """
     Downloads and prepares a .csv file containing all countries and
     its capitals of the world.
 
-    Data is downloaded from SimpleMaps.com (https://simplemaps.com/data/world-cities)
+    Data is fetched from geonames.org (https://www.geonames.org/)
     and is licenced under CC BY 4.0 (https://creativecommons.org/licenses/by/4.0/).
 
     Parameters
@@ -51,49 +73,108 @@ def preprocess_country_capitals(raw_data_dir: str, output_dir: str) -> None:
         Raw data directory
     output_dir : str
         Directory to save output data.
+    geonames_username : str
+        GeoNames username
     """
     # Constants
-    data_url = "https://simplemaps.com/static/data/world-cities/basic/simplemaps_worldcities_basicv1.71.zip"
-    raw_data_zip_filepath = join(raw_data_dir, "world-cities.zip")
-    raw_data_extracted_zip_dir = join(raw_data_dir, "world-cities")
-    raw_data_world_cities_csv = join(raw_data_extracted_zip_dir, "worldcities.csv")
-    output_filepath = join(output_dir, "country_capitals.csv")
+    all_countries_combined_data_url = (
+        "https://download.geonames.org/export/dump/allCountries.zip"
+    )
+    all_countries_raw_data_zip_filepath = join(raw_data_dir, "allCountries.zip")
+    all_countries_raw_data_txt_filepath = join(raw_data_dir, "allCountries.txt")
+    country_info_csv_data_url = (
+        f"https://secure.geonames.org/countryInfoCSV?username={geonames_username}"
+    )
+    country_info_raw_data_csv_filepath = join(raw_data_dir, "country-info.csv")
+    output_filepath = join(output_dir, "country-info.csv")
 
-    # Download raw zip
-    if not isfile(raw_data_zip_filepath):
-        print(f"Downloading raw world-cities data...")
-        download_from_url(url=data_url, destination_filepath=raw_data_zip_filepath)
+    # Download raw data
+    if not isfile(all_countries_raw_data_zip_filepath):
+        print("Downloading raw country data...")
+        download_from_url(
+            all_countries_combined_data_url, all_countries_raw_data_zip_filepath
+        )
         print("Done!")
 
-    # Extract raw data if not present
-    if not isdir(raw_data_extracted_zip_dir):
+    if not isfile(all_countries_raw_data_txt_filepath):
         print("Extracting raw data...")
-        with zipfile.ZipFile(raw_data_zip_filepath, "r") as zip_file:
-            zip_file.extractall(raw_data_extracted_zip_dir)
+        with zipfile.ZipFile(all_countries_raw_data_zip_filepath, "r") as zip_file:
+            zip_file.extractall(raw_data_dir)
         print("Done!")
 
-    # Extract capital cities only
-    world_cities_df = pd.read_csv(raw_data_world_cities_csv)
-    world_cities_df_capitals = world_cities_df[world_cities_df["capital"] == "primary"]
+    if not isfile(country_info_raw_data_csv_filepath):
+        print("Downloading country info data...")
+        download_from_url(country_info_csv_data_url, country_info_raw_data_csv_filepath)
+        print("Done!")
 
-    # We only want the columns: country, city, lat, lng
-    world_cities_df_capitals = world_cities_df_capitals[["country", "city", "lat", "lng"]]
+    # Load raw data into Pandas DataFrames and join them
+    all_countries_info_df = pd.read_csv(
+        all_countries_raw_data_txt_filepath,
+        sep="\t",
+        na_filter=False,
+        header=None,
+        names=[
+            "geonameId",
+            "name",
+            "asciiname",
+            "alternatenames",
+            "latitude",
+            "longitude",
+            "feature class",
+            "feature code",
+            "country code",
+            "cc2",
+            "admin1 code",
+            "admin2 code",
+            "admin3 code",
+            "admin4 code",
+            "population",
+            "elevation",
+            "dem",
+            "timezone",
+            "modification date",
+        ],
+        usecols=["geonameId", "latitude", "longitude"],
+        index_col="geonameId",
+    )
+    country_info_df = pd.read_csv(
+        country_info_raw_data_csv_filepath,
+        sep="\t",
+        na_filter=False,
+        usecols=["name", "capital", "continent", "geonameId"],
+    )
+    country_info_df = country_info_df.join(
+        all_countries_info_df, on="geonameId", how="left"
+    )
 
-    # Apply preprocessing to country/capital names
-    preprocess_name = lambda name: " ".join(preprocess_text(name)).replace(" ", "_")
-    world_cities_df_capitals["country"] = world_cities_df_capitals["country"].apply(
-        preprocess_name
+    # Remove unused GeoNameId column
+    country_info_df.drop("geonameId", inplace=True, axis=1)
+
+    # Replace continent codes with names
+    continent_code_to_name = {
+        "AF": "Africa",
+        "AS": "Asia",
+        "EU": "Europe",
+        "NA": "North America",
+        "OC": "Oceania",
+        "SA": "South America",
+        "AN": "Antarctica",
+    }
+    country_info_df["continent"] = country_info_df["continent"].apply(
+        lambda code: continent_code_to_name[code]
     )
-    world_cities_df_capitals["city"] = world_cities_df_capitals["city"].apply(
-        preprocess_name
-    )
-    world_cities_df_capitals.rename(columns={"city": "capital"}, inplace=True)
+
+    # Apply preprocessing to country name and capital
+    country_info_df["name"] = country_info_df["name"].apply(preprocess_name)
+    country_info_df["capital"] = country_info_df["capital"].apply(preprocess_name)
 
     # Save to file
-    world_cities_df_capitals.to_csv(output_filepath, index=False)
+    country_info_df.to_csv(output_filepath, index=False)
 
 
-def preprocess_analysis_data(raw_data_dir: str, output_dir: str) -> None:
+def preprocess_analysis_data(
+    raw_data_dir: str, output_dir: str, geonames_username: str
+) -> None:
     """
     Preprocesses data for analysing word embeddings
 
@@ -103,16 +184,22 @@ def preprocess_analysis_data(raw_data_dir: str, output_dir: str) -> None:
         Raw data directory
     output_dir : str
         Directory to save analysis data.
+    geonames_username : str
+        GeoNames username (create account here: https://www.geonames.org/login)
     """
     # Ensure raw data/output directories exist
     makedirs(raw_data_dir, exist_ok=True)
     makedirs(output_dir, exist_ok=True)
 
-    print("-- Country/capitals --")
-    preprocess_country_capitals(raw_data_dir, output_dir)
+    print("-- Country info --")
+    preprocess_country_info(raw_data_dir, output_dir, geonames_username)
     print("Done!")
 
 
 if __name__ == "__main__":
     args = parse_args()
-    preprocess_analysis_data(raw_data_dir=args.raw_data_dir, output_dir=args.output_dir)
+    preprocess_analysis_data(
+        raw_data_dir=args.raw_data_dir,
+        output_dir=args.output_dir,
+        geonames_username=args.geonames_username,
+    )
