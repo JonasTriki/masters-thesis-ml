923415eed9846900c8c3b5fd5472955e73162207
Jonas Triki <trikijonas@gmail.com>
2020-12-04 23:20:18 +0100

    Create functions for word2vec analysis

--- code/analysis_of_embeddings/analysis_utils.py
+++ code/analysis_of_embeddings/analysis_utils.py
@@ -1,6 +1,100 @@
+from os.path import join
+
+import joblib
 import numpy as np
+from hdbscan import HDBSCAN
 from matplotlib import pyplot as plt
 from sklearn.cluster import AgglomerativeClustering
+from sklearn.model_selection import ParameterGrid
+from tqdm import tqdm
+
+
+def k_means_cluster_hyperparameter_search():
+    # TODO: Implement this
+    pass
+
+
+def k_means_mini_batch_cluster_hyperparameter_search():
+    # TODO: Implement this
+    pass
+
+
+def k_medoids_cluster_hyperparameter_search():
+    # TODO: Implement this
+    pass
+
+
+def gmm_cluster_hyperparameter_search():
+    # TODO: Implement this
+    pass
+
+
+def agglomerative_cluster_hyperparameter_search():
+    # TODO: Implement this
+    pass
+
+
+def hdbscan_cluster_hyperparameter_search(
+    param_grid: ParameterGrid,
+    default_params: dict,
+    word_embeddings: np.ndarray,
+    output_dir: str,
+    model_name: str,
+    dataset_name: str,
+    output_filepath_suffix: str,
+) -> dict:
+    """
+    Searches for the best set of hyperparameters when using the
+    HDBSCAN clustering algorithm.
+
+    Parameters
+    ----------
+    param_grid : ParameterGrid
+        Parameter grid to search through
+    default_params : dict
+        Default parameters for HDBSCAN
+    word_embeddings : np.ndarray
+        Word embeddings to perform clustering on
+    output_dir : str
+        Output directory
+    model_name : str
+        Name of the model
+    dataset_name : str
+        Name of the dataset the model was trained on
+    output_filepath_suffix : str
+        Output filepath suffix
+
+    Returns
+    -------
+    result : dict
+        Dictionary containing cluster labels and DBCV scores
+    """
+    # Perform clustering
+    hdbscan_cluster_labels = []
+    hdbscan_dbcv_scores = []
+    for param_grid in tqdm(param_grid, desc="Performing HDBSCAN clustering"):
+        hdbscan_clustering = HDBSCAN(**param_grid, **default_params)
+        cluster_labels_pred = hdbscan_clustering.fit_predict(word_embeddings)
+        hdbscan_cluster_labels.append(cluster_labels_pred)
+
+        # Use already computed DBCV score to compare parameters
+        dbcv_score = hdbscan_clustering.relative_validity_
+        hdbscan_dbcv_scores.append(dbcv_score)
+
+    # Create result as dict
+    result = {
+        "cluster_labels": hdbscan_cluster_labels,
+        "dbcv_scores": hdbscan_dbcv_scores,
+        "best_labels_idx": np.argmax(hdbscan_dbcv_scores),
+    }
+
+    # Save to output dir
+    output_path = join(
+        output_dir, f"{model_name}-{dataset_name}-{output_filepath_suffix}.joblib"
+    )
+    joblib.dump(result, output_path)
+
+    return result
 
 
 def create_linkage_matrix(clustering: AgglomerativeClustering) -> list:
@@ -65,6 +159,7 @@ def plot_cluster_metric_scores(
     best_score_idx: int,
     metric_name: str,
     scatter: bool = True,
+    ax: plt.axis = None,
 ) -> None:
     """
     Plots internal cluster validation metric scores
@@ -80,8 +175,12 @@ def plot_cluster_metric_scores(
     metric_name : str
         Name of the internal cluster validation metric
     scatter : bool
-        Whether or not to scatter points
+        Whether or not to scatter points (defaults to True)
+    ax : plt.axis
+        Matplotlib axis (defaults to None)
     """
+    if ax is None:
+        _, ax = plt.subplots()
     xs = range(len(hyperparameters))
     plt.plot(xs, scores)
     if scatter:
@@ -89,7 +188,8 @@ def plot_cluster_metric_scores(
         plt.scatter(xs[best_score_idx], scores[best_score_idx], c="r")
     plt.xticks(xs, hyperparameters, rotation=90)
     plt.xlabel("Hyperparameters")
-    plt.ylabel(metric_name)
+    plt.ylabel(f"{metric_name} score")
+    plt.tight_layout()
     plt.show()
 
 
