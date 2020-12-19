0a7efdbdd55366bc604e03e02cd84dc837bf486e
Jonas Triki <trikijonas@gmail.com>
2020-12-19 18:23:45 +0100

    Update preprocess data analysis scipt

---
+++ code/analysis_of_embeddings/preprocess_analysis_data_enwiki.sh
@@ -0,0 +1,6 @@
+python preprocess_analysis_data.py \
+--raw_data_dir raw_data \
+--custom_data_dir custom_data \
+--output_dir data \
+--geonames_username jtr008 \
+--words_filepath ../output/word2vec_training/word2vec_enwiki_sept_2020_word2phrase/word2vec_enwiki_words.txt
