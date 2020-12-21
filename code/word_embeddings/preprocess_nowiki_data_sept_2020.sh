e14bcf99a8588beabf27c3472de4ec4d186b14c7
Jonas Triki <trikijonas@gmail.com>
2020-12-22 00:53:39 +0100

    Update notebook and create new preprocessing script for nowiki

---
+++ code/word_embeddings/preprocess_nowiki_data_sept_2020.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+python preprocess_wikipedia_data.py \
+--language norwegian \
+--wiki_dump_time 20200901 \
+--raw_data_dir raw_data \
+--output_dir data/enwiki-20200901 \
+--min_sent_word_count 5 \
+--num_output_files -1 \
+--max_wikipedia_files -1
