98f1c111eb9d43700b21d571342671d1089345bf
Jonas Triki <trikijonas@gmail.com>
2021-01-05 20:12:48 +0100

    Add internal cluster validation metrics

--- code/analysis_of_embeddings/cluster_analysis_metrics.py
+++ code/analysis_of_embeddings/cluster_analysis_metrics.py
@@ -1,8 +1,8 @@
 import numpy as np
 from cdbw import CDbw
 from hdbscan import HDBSCAN
-from s_dbw import S_Dbw
-from sklearn.metrics import silhouette_score
+from s_dbw import SD, S_Dbw
+from sklearn.metrics import davies_bouldin_score, silhouette_score
 
 
 def silhouette_score_metric(
@@ -37,6 +37,38 @@ def silhouette_score_metric(
     return "Silhouette Coefficient", metric_score, True
 
 
+def davies_bouldin_score_metric(
+    word_embeddings: np.ndarray, cluster_labels: np.ndarray, clusterer, **_: dict
+) -> tuple:
+    """
+    Wrapper function for Davies–Bouldin index used in `cluster_analysis` function.
+
+    Parameters
+    ----------
+    word_embeddings : np.ndarray
+        Word embeddings/pairwise distances to evaluate on
+    cluster_labels : np.ndarray
+        Predicted cluster labels
+    clusterer : clustering instance
+        Clustering instance (fitted on data)
+    **_ : dict
+        Keyword arguments sent into the void (not used)
+
+    Returns
+    -------
+    result : tuple
+        Result as a triple, consisting of metric title, metric score and whether or not
+        the metrics objective function is to maximize the metric score.
+    """
+    # Compute metric scores
+    if cluster_labels is None:
+        cluster_labels = clusterer.labels_
+    metric_score = davies_bouldin_score(X=word_embeddings, labels=cluster_labels)
+
+    # Return tuple with result
+    return "Davies–Bouldin index", metric_score, False
+
+
 def s_dbw_score_metric(
     word_embeddings: np.ndarray, cluster_labels: np.ndarray, clusterer, **kwargs: dict
 ) -> tuple:
@@ -69,6 +101,38 @@ def s_dbw_score_metric(
 
     # Return tuple with result
     return "S_Dbw validity index", metric_score, False
+
+
+def sd_score_metric(
+    word_embeddings: np.ndarray, cluster_labels: np.ndarray, clusterer, **kwargs: dict
+) -> tuple:
+    """
+    Wrapper function for SD score used in `cluster_analysis` function.
+
+    Parameters
+    ----------
+    word_embeddings : np.ndarray
+        Word embeddings/pairwise distances to evaluate on
+    cluster_labels : np.ndarray
+        Predicted cluster labels
+    clusterer : clustering instance
+        Clustering instance (fitted on data)
+    **kwargs : dict
+        Keyword arguments passed to `SD`
+
+    Returns
+    -------
+    result : tuple
+        Result as a triple, consisting of metric title, metric score and whether or not
+        the metrics objective function is to maximize the metric score.
+    """
+    # Compute metric scores
+    if cluster_labels is None:
+        cluster_labels = clusterer.labels_
+    metric_score = SD(X=word_embeddings, labels=cluster_labels, metric="cosine", **kwargs)
+
+    # Return tuple with result
+    return "SD validity index", metric_score, False
 
 
 def cdbw_score_metric(
