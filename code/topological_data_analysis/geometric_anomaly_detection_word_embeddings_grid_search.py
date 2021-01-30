3b37f0184265b6fb07b0aab3d8486c7e7c5fd1a5
Jonas Triki <trikijonas@gmail.com>
2021-01-30 16:44:42 +0100

    Update grid search script

--- code/topological_data_analysis/geometric_anomaly_detection_word_embeddings_grid_search.py
+++ code/topological_data_analysis/geometric_anomaly_detection_word_embeddings_grid_search.py
@@ -1,4 +1,20 @@
 import argparse
+import sys
+from os import makedirs
+from os.path import isfile, join
+
+import joblib
+import numpy as np
+import ripser_plusplus_python as rpp_py
+from geometric_anomaly_detection import (
+    GeometricAnomalyDetection,
+    grid_search_prepare_word_ints_within_radii,
+)
+from sklearn.metrics import euclidean_distances
+
+sys.path.append("..")
+
+from word_embeddings.word2vec import load_model_training_output
 
 
 def parse_args() -> argparse.Namespace:
@@ -15,10 +31,13 @@ def parse_args() -> argparse.Namespace:
         "--model_dir",
         type=str,
         default="",
-        help="Directory of the model to evaluate",
+        help="Directory of the model to load",
     )
     parser.add_argument(
-        "--model_name", type=str, default="word2vec", help="Name of the trained model"
+        "--model_name",
+        type=str,
+        default="word2vec",
+        help="Name of the trained word2vec model",
     )
     parser.add_argument(
         "--dataset_name",
@@ -27,15 +46,143 @@ def parse_args() -> argparse.Namespace:
         help="Name of the dataset the model is trained on",
     )
     parser.add_argument(
+        "--vocab_size",
+        type=int,
+        help="Size of the vocabulary to use",
+    )
+    parser.add_argument(
+        "--manifold_dimension",
+        type=int,
+        help="Manifold dimension to be passed to geometric anomaly detection algorithm",
+    )
+    parser.add_argument(
+        "--num_radii_to_use",
+        type=int,
+        help="Number of radii parameters to use at most (all for outer radius and (all - 1) for inner radius)",
+    )
+    parser.add_argument(
+        "--max_annulus_radii_diff",
+        type=float,
+        default=np.inf,
+        help="Maximal difference between outer and inner radii for annulus",
+    )
+    parser.add_argument(
         "--output_dir",
         type=str,
         default="",
-        help="Output directory to save processed data",
+        help="Output directory to save data",
     )
     return parser.parse_args()
 
 
+def geometric_anomaly_detection_grid_search(
+    model_dir: str,
+    model_name: str,
+    dataset_name: str,
+    vocab_size: int,
+    manifold_dimension: int,
+    num_radii_to_use: int,
+    max_annulus_radii_diff: float,
+    output_dir: str,
+) -> None:
+    """
+    Performs grid search to find best set of annulus radii (inner and outer)
+    for computing geometric data anomaly detection on word embeddings.
+
+    Parameters
+    ----------
+    model_dir : str
+        Directory of the model to load.
+    model_name : str
+        Name of the trained word2vec model.
+    dataset_name : str
+        Name of the dataset the model is trained on.
+    vocab_size : int
+        Size of the vocabulary to use.
+    manifold_dimension : int
+        Manifold dimension to be passed to geometric anomaly detection algorithm.
+    num_radii_to_use : int
+        Number of radii parameters to use at most
+        (all for outer radius and (all - 1) for inner radius).
+    max_annulus_radii_diff : float
+        Maximal difference between outer and inner radii for annulus
+    output_dir : str
+        Output directory to save data
+    """
+    # Ensure output directory exists
+    makedirs(output_dir, exist_ok=True)
+
+    # Load output from training word2vec
+    w2v_training_output = load_model_training_output(
+        model_training_output_dir=model_dir,
+        model_name=model_name,
+        dataset_name=dataset_name,
+    )
+    last_embedding_weights = w2v_training_output["last_embedding_weights"]
+    model_id = f"{model_name}_{dataset_name}"
+
+    # Normalize word embeddings
+    last_embedding_weights_normalized = last_embedding_weights / np.linalg.norm(
+        last_embedding_weights, axis=1
+    ).reshape(-1, 1)
+
+    # Compute pairwise distances for grid search using specified vocab size
+    vocabulary_word_ints = list(range(vocab_size))
+    word_embeddings_pairwise_dists_grid_search = euclidean_distances(
+        last_embedding_weights_normalized[vocabulary_word_ints]
+    )
+
+    # Precompute word ints within each radii
+    words_within_radii_result_filepath = join(output_dir, f"{model_id}_wwr.joblib")
+    if not isfile(words_within_radii_result_filepath):
+        words_within_radii_result = grid_search_prepare_word_ints_within_radii(
+            word_ints=vocabulary_word_ints,
+            num_radii_per_parameter=num_radii_to_use,
+            word_vector_distance=lambda i, j: word_embeddings_pairwise_dists_grid_search[
+                i, j
+            ],
+            word_embeddings_pairwise_dists=word_embeddings_pairwise_dists_grid_search,
+        )
+        joblib.dump(words_within_radii_result, words_within_radii_result_filepath)
+    else:
+        words_within_radii_result = joblib.load(words_within_radii_result_filepath)
+    word_ints_within_radii, radii_space = words_within_radii_result
+
+    # Initialize GAD instance
+    gad_instance = GeometricAnomalyDetection(
+        word_embeddings=last_embedding_weights_normalized
+    )
+
+    # Do grid search
+    grid_search_result_filepath = join(
+        output_dir, f"{model_id}_grid_search_result.joblib"
+    )
+    best_gad_result_idx, gad_results, P_man_counts = gad_instance.grid_search_radii(
+        word_ints=vocabulary_word_ints,
+        manifold_dimension=manifold_dimension,
+        num_radii_per_parameter=num_radii_to_use,
+        outer_inner_radii_max_diff=max_annulus_radii_diff,
+        word_ints_within_radii=word_ints_within_radii,
+        radii_space=radii_space,
+        word_embeddings_pairwise_dists=word_embeddings_pairwise_dists_grid_search,
+    )
+    grid_search_result = {
+        "best_gad_result_idx": best_gad_result_idx,
+        "gad_results": gad_results,
+        "P_man_counts": P_man_counts,
+    }
+    joblib.dump(grid_search_result, grid_search_result_filepath)
+
+
 if __name__ == "__main__":
     args = parse_args()
-
-    pass
+    geometric_anomaly_detection_grid_search(
+        model_dir=args.model_dir,
+        model_name=args.model_name,
+        dataset_name=args.dataset_name,
+        vocab_size=args.vocab_size,
+        manifold_dimension=args.manifold_dimension,
+        num_radii_to_use=args.num_radii_to_use,
+        max_annulus_radii_diff=args.max_annulus_radii_diff,
+        output_dir=args.output_dir,
+    )
---
+++ code/topological_data_analysis/geometric_anomaly_detection_word_embeddings_grid_search.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+python geometric_anomaly_detection_word_embeddings_grid_search.py \
+--model_dir ../output/word2vec_training/word2vec_enwiki_sept_2020_word2phrase \
+--model_name word2vec \
+--dataset_name enwiki \
+--output_dir data
