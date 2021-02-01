cc0700540662f9e0e82b9011215869871269cce1
Jonas Triki <trikijonas@gmail.com>
2021-02-01 13:41:49 +0100

    Add function for generating points in spheres

--- code/topological_data_analysis/tda_utils.py
+++ code/topological_data_analysis/tda_utils.py
@@ -216,3 +216,92 @@ def tps(
         return wasserstein_norm, barcodes
     else:
         return wasserstein_norm
+
+
+def generate_points_in_spheres(
+    num_points: int,
+    sphere_dimensionality: int,
+    sphere_means: tuple,
+    space_dimensionality: int = None,
+    random_state: int = 0,
+) -> tuple:
+    """
+    Generates points laying in two d-dimensional spheres. Spheres can be overlapping
+    by setting sphere_means accordingly (e.g. [0, 0.75]).
+
+    Parameters
+    ----------
+    num_points : int
+        Number of points to generate per sphere.
+    sphere_dimensionality : int
+        Dimensionality (d) to use when generating points in d-dimensional spheres.
+    sphere_means : tuple
+        Tuple containing two floats indicating the mean of each sphere.
+    space_dimensionality : int, optional
+        Dimensionality to use for the point space (must be equal or greater than
+        sphere_dimensionality). Can be used to increase the dimensionality for the points.
+        Defaults to None (or sphere_dimensionality).
+    random_state : int, optional
+        Random state to use when generating points (defaults to 0).
+
+    Returns
+    -------
+    result : tuple
+        Tuple containing randomly generated sphere points and which sphere the point
+        corresponds to (2 indicates intersection between spheres).
+    """
+    # Set random seed
+    np.random.seed(random_state)
+
+    # Generate points in spheres
+    sphere_means_in_space_dim = (
+        np.repeat(mean, sphere_dimensionality) for mean in sphere_means
+    )
+    total_num_points = 2 * num_points
+    if space_dimensionality is not None:
+        sphere_means_in_space_dim = (
+            np.concatenate(
+                (sphere_mean, np.zeros(space_dimensionality - sphere_dimensionality))
+            )
+            for sphere_mean in sphere_means_in_space_dim
+        )
+
+        sphere_points = np.zeros((total_num_points, space_dimensionality))
+    else:
+        sphere_points = np.zeros((total_num_points, sphere_dimensionality))
+    sphere_point_labels = np.zeros(total_num_points)
+
+    for i, loc in enumerate(sphere_means_in_space_dim):
+        for j in range(num_points):
+            sphere_point_idx = i * num_points + j
+
+            # Method 20 from (accessed 31th of January 2021):
+            # http://extremelearning.com.au/how-to-generate-uniformly-random-points-on-n-spheres-and-n-balls/
+            u = np.random.normal(loc=0, scale=1, size=sphere_dimensionality)
+            u /= np.linalg.norm(u)
+            r = np.random.random() ** (1.0 / sphere_dimensionality)
+            x = r * u
+            if space_dimensionality is not None:
+                x = np.concatenate(
+                    (x, np.zeros(space_dimensionality - sphere_dimensionality))
+                )
+
+            x += loc  # Shift point by adding mean
+            sphere_points[sphere_point_idx] = x
+
+            # Compute distance to sphere origos
+            point_in_spheres = []
+            for k, sphere_loc in enumerate(sphere_means_in_space_dim):
+                dist_to_sphere = np.linalg.norm(x - sphere_loc)
+                point_in_sphere = dist_to_sphere <= 1
+                point_in_spheres.append(point_in_sphere)
+
+            # If point is between spheres, we label it as 2, indicating overlapping.
+            if all(point_in_spheres):
+                sphere_point_labels[sphere_point_idx] = 2
+            else:
+
+                # Else, use sphere index as label.
+                sphere_point_labels[sphere_point_idx] = point_in_spheres.index(True)
+
+    return sphere_points, sphere_point_labels
