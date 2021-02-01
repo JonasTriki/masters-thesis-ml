41acc063a56b9116d4f7e4fcbb17fae61cbf0357
Jonas Triki <trikijonas@gmail.com>
2021-02-01 19:48:30 +0100

    Update notebooks, update wikipedia script to Jan 2021 and create script for saving country/capital PCA plot

--- code/word_embeddings/preprocess_enwiki_data_sept_2020.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-python preprocess_wikipedia_data.py \
---language english \
---wiki_dump_time 20200901 \
---raw_data_dir raw_data \
---output_dir data/enwiki-20200901 \
---min_sent_word_count 5 \
---num_output_files -1 \
---max_wikipedia_files -1
