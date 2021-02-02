6e379116f83c3458cd1454bda57f42f931db6aca
Jonas Triki <trikijonas@gmail.com>
2021-02-02 12:52:17 +0100

    Update word2phrase script

--- code/word_embeddings/train_word2phrase_enwiki.sh
+++ code/word_embeddings/train_word2phrase_enwiki.sh
@@ -1,6 +1,6 @@
 python train_word2phrase.py \
---text_data_dir data/enwiki-20200901 \
---dataset_name enwiki-20200901 \
+--text_data_dir data/enwiki-20210101 \
+--dataset_name enwiki-20210101 \
 --starting_epoch_nr 1 \
 --n_epochs 2 \
 --max_vocab_size -1 \
