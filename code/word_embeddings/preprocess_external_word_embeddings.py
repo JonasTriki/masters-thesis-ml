e845bd2054723e66b206d453162d97ad0aa55c2a
Jonas Triki <trikijonas@gmail.com>
2021-02-05 21:36:23 +0100

    Change to Gzip

--- code/word_embeddings/preprocess_external_word_embeddings.py
+++ code/word_embeddings/preprocess_external_word_embeddings.py
@@ -1,6 +1,6 @@
 import argparse
+import gzip
 import sys
-import tarfile
 from os import makedirs
 from os.path import isfile, join
 
@@ -86,10 +86,8 @@ def preprocess_external_word_embeddings(raw_data_dir: str, output_dir: str) -> N
 
     if not isfile(google_news_vectors_bin_raw_filepath):
         print(f"Extracting {google_news_vectors_zip_raw_filename}...")
-        with tarfile.open(google_news_vectors_zip_raw_filepath) as tar_file:
-            tar_file.extract(
-                google_news_vectors_bin_raw_filename, google_news_vectors_bin_raw_filepath
-            )
+        with gzip.GzipFile(google_news_vectors_zip_raw_filepath) as gzip_file:
+            gzip_file.write(google_news_vectors_bin_raw_filepath)
         print("Done!")
 
     # Parse vectors from binary file and save result
