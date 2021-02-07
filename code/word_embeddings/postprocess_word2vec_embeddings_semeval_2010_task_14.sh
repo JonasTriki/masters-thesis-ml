d2cceaa9424f7353cbaae0ab387e53884fe37944
Jonas Triki <trikijonas@gmail.com>
2021-02-07 21:52:02 +0100

    Fix script path

--- code/word_embeddings/postprocess_word2vec_embeddings_semeval_2010_task_14.sh
+++ code/word_embeddings/postprocess_word2vec_embeddings_semeval_2010_task_14.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 screen -dmS postprocess_word2vec_semeval_2010_task_14 -L -Logfile postprocess_word2vec_semeval_2010_task_14.logs python postprocess_word2vec_embeddings.py \
---model_training_output_dir ../output/word2vec_training/word2vec_semeval_2010_task_14_jan_2021_word2phrase \
+--model_training_output_dir ../output/word2vec_training/word2vec_semeval_2010_task_14 \
 --model_name word2vec \
 --dataset_name semeval_2010_task_14 \
 --vocab_size -1 \
