01dc93499b5d9d678ad97413587cf499b7931236
Jonas Triki <trikijonas@gmail.com>
2021-02-16 17:26:40 +0100

    Update notebooks

--- code/topological_data_analysis/geometric_anomaly_detection_word_embeddings_grid_search_zoomed.sh
+++ code/topological_data_analysis/geometric_anomaly_detection_word_embeddings_grid_search_zoomed.sh
@@ -6,9 +6,10 @@ screen -dmS gad_word_embeddings_grid_search -L -Logfile gad_word_embeddings_grid
 --vocab_size 10000 \
 --annoy_index_filepath ../output/word2vec_training/word2vec_enwiki_jan_2021_word2phrase/word2vec_enwiki_05_10_weights_annoy_index.ann \
 --manifold_dimension 2 \
---num_radii_to_use 50 \
+--num_radii_to_use 10 \
 --min_outer_annulus_radius 1.142065 \
 --max_outer_annulus_radius 1.446615 \
+--num_cpus 10 \
 --output_dir data \
 --output_filepath_suffix zoomed
 screen -r gad_word_embeddings_grid_search
