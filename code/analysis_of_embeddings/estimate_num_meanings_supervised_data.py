5dc6d04dc6cce2ba31965b53f8f8dd59379e3e77
Jonas Triki <trikijonas@gmail.com>
2021-03-21 19:53:14 +0100

    Update supervised model script and notebook

--- code/analysis_of_embeddings/estimate_num_meanings_supervised_data.py
+++ code/analysis_of_embeddings/estimate_num_meanings_supervised_data.py
@@ -1,5 +1,5 @@
 import sys
-from os.path import isfile
+from os.path import isfile, join
 from typing import Optional
 
 import joblib
@@ -24,7 +24,7 @@ lpca_n_neighbors = 100
 tps_neighbourhood_sizes = np.linspace(start=10, stop=100, num=10, dtype=int)
 compute_words_to_num_synsets = False
 compute_id = False
-compute_tps = True
+compute_tps = False
 compute_word_meaning_features = True
 
 # Load word embeddings.
@@ -41,6 +41,28 @@ last_embedding_weights_normalized = w2v_training_output[
 ]
 words = w2v_training_output["words"]
 word_to_int = w2v_training_output["word_to_int"]
+
+# Load SemEval-2010 task 14 words
+semeval_2010_14_word_senses = joblib.load(
+    join(
+        "..", "topological_data_analysis", "data", "semeval_2010_14_word_senses.joblib"
+    )
+)
+semeval_target_words = np.array(list(semeval_2010_14_word_senses["all"].keys()))
+semeval_target_words_in_vocab_filter = [
+    i for i, word in enumerate(semeval_target_words) if word in word_to_int
+]
+semeval_target_words_in_vocab = semeval_target_words[
+    semeval_target_words_in_vocab_filter
+]
+semeval_gs_clusters = np.array(list(semeval_2010_14_word_senses["all"].values()))
+semeval_gs_clusters_in_vocab = semeval_gs_clusters[semeval_target_words_in_vocab_filter]
+semeval_gs_clusters_dict = {
+    word: gs_meanings
+    for word, gs_meanings in zip(
+        semeval_target_words_in_vocab, semeval_gs_clusters_in_vocab
+    )
+}
 
 
 def find_synset_words(word: str) -> Optional[int]:
@@ -70,6 +92,12 @@ else:
 data_words = list(words_to_num_synsets.keys())
 data_word_ints = np.array([word_to_int[word] for word in data_words])
 
+j = 0
+for word in semeval_target_words_in_vocab:
+    if word in data_words:
+        j += 1
+print(j, j == len(semeval_target_words_in_vocab))
+
 # Estimate the intrinsic dimension (ID) for each word vector
 if compute_id and compute_word_meaning_features:
     print("Estimating the intrinsic dimension (ID)...")
@@ -88,6 +116,8 @@ if compute_tps and compute_word_meaning_features:
     last_embedding_weights_scann_instance = w2v_training_output[
         "last_embedding_weights_scann_instance"
     ]
+    tps_scores = {}
+    tps_pds = {}
     for tps_neighbourhood_size in tps_neighbourhood_sizes:
         print(f"Neighbourhood size: {tps_neighbourhood_size}")
         tps_scores_filepath = (
@@ -97,8 +127,8 @@ if compute_tps and compute_word_meaning_features:
         if isfile(tps_scores_filepath) and isfile(tps_pds_filepath):
             continue
 
-        tps_scores = []
-        tps_pds = []
+        tps_scores[tps_neighbourhood_size] = []
+        tps_pds[tps_neighbourhood_size] = []
         for i, word in enumerate(tqdm(data_words)):
             word_i = word_to_int[word]
             tps_score, tps_pd = tps(
@@ -122,32 +152,33 @@ if compute_tps and compute_word_meaning_features:
                 ]
             )
 
-            tps_scores.append(tps_score)
-            tps_pds.append(tps_pd_zero_dim)
+            tps_scores[tps_neighbourhood_size].append(tps_score)
+            tps_pds[tps_neighbourhood_size].append(tps_pd_zero_dim)
 
         # Save result
-        np.save(tps_scores_filepath, np.array(tps_scores))
-        np.save(tps_pds_filepath, np.array(tps_pds))
+        np.save(tps_scores_filepath, np.array(tps_scores[tps_neighbourhood_size]))
+        np.save(tps_pds_filepath, np.array(tps_pds[tps_neighbourhood_size]))
     print("Done!")
 else:
     tps_scores = {
-        [n_size]: np.load(f"data/tps_{n_size}_scores_wordnet_enwiki.npy")
+        n_size: np.load(f"data/tps_{n_size}_scores_wordnet_enwiki.npy")
         for n_size in tps_neighbourhood_sizes
     }
     tps_pds = {
-        [n_size]: np.load(f"data/tps_{n_size}_pds_wordnet_enwiki.npy")
+        n_size: np.load(f"data/tps_{n_size}_pds_wordnet_enwiki.npy")
         for n_size in tps_neighbourhood_sizes
     }
     print("Loaded tps_scores and tps_pds!")
 
 
 def create_word_meaning_model_data_features(
-    words: list,
+    target_words: list,
+    all_words: list,
     word_to_int: dict,
     tps_scores: dict,
     tps_pds: dict,
     words_estimated_ids: np.array,
-    words_to_num_synsets: dict,
+    words_to_meanings: dict,
 ) -> pd.DataFrame:
     """
     TODO: Docs
@@ -161,14 +192,15 @@ def create_word_meaning_model_data_features(
     for n_size in tps_neighbourhood_sizes:
         data_features[f"tps_{n_size}"] = []
         data_features[f"tps_{n_size}_bottle"] = []
-    for i, word in enumerate(tqdm(words)):
-        word_i = word_to_int[word]
+
+    for target_word in tqdm(target_words):
+        i = all_words.index(target_word)
 
         # Add word and word integer
-        data_features["word"].append(word)  # Word
-        data_features["word_int"].append(word_i)  # Word integer
+        data_features["word"].append(target_word)  # Word
+        data_features["word_int"].append(word_to_int[target_word])  # Word integer
         data_features["estimated_id"].append(words_estimated_ids[i])  # Estimated ID
-        data_features["y"].append(words_to_num_synsets[word])
+        data_features["y"].append(words_to_meanings[target_word])
 
         for n_size in tps_neighbourhood_sizes:
             data_features[f"tps_{n_size}"].append(tps_scores[n_size][i])
@@ -187,15 +219,28 @@ def create_word_meaning_model_data_features(
 
 
 if compute_word_meaning_features:
-    data_X_df = create_word_meaning_model_data_features(
-        words=data_words,
+    train_data_df = create_word_meaning_model_data_features(
+        target_words=data_words,
+        all_words=data_words,
         word_to_int=word_to_int,
         tps_scores=tps_scores,
         tps_pds=tps_pds,
         words_estimated_ids=words_estimated_ids,
-        words_to_num_synsets=words_to_num_synsets,
+        words_to_meanings=words_to_num_synsets,
     )
-    data_X_df.to_csv("data/word_meaning_features.csv", index=False)
+    train_data_df.to_csv("data/word_meaning_train_data.csv", index=False)
+    test_data_df = create_word_meaning_model_data_features(
+        target_words=semeval_target_words_in_vocab,
+        all_words=data_words,
+        word_to_int=word_to_int,
+        tps_scores=tps_scores,
+        tps_pds=tps_pds,
+        words_estimated_ids=words_estimated_ids,
+        words_to_meanings=semeval_gs_clusters_dict,
+    )
+    test_data_df.to_csv("data/word_meaning_test_data.csv", index=False)
 else:
-    data_X_df = pd.read_csv("data/word_meaning_features.csv")
-print(data_X_df)
+    train_data_df = pd.read_csv("data/word_meaning_train_data.csv")
+    test_data_df = pd.read_csv("data/word_meaning_test_data.csv")
+print("Train", train_data_df)
+print("Test", test_data_df)
