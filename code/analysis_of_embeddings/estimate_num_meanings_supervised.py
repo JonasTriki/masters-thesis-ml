84c35bac2fbe50f82978e890b35b1c691af474f4
Jonas Triki <trikijonas@gmail.com>
2021-04-02 23:53:50 +0200

    Add argparse and some docs

--- code/analysis_of_embeddings/estimate_num_meanings_supervised.py
+++ code/analysis_of_embeddings/estimate_num_meanings_supervised.py
@@ -1,3 +1,6 @@
+import argparse
+from os.path import join
+
 import joblib
 import numpy as np
 import pandas as pd
@@ -8,7 +11,36 @@ rng_seed = 399
 np.random.seed(rng_seed)
 
 
+def parse_args() -> argparse.Namespace:
+    """
+    Parses arguments sent to the python script.
+
+    Returns
+    -------
+    parsed_args : argparse.Namespace
+        Parsed arguments
+    """
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--train_data_filepath",
+        type=str,
+        default="",
+        help="Filepath of word meaning training data",
+    )
+    parser.add_argument(
+        "--output_dir",
+        type=str,
+        default="",
+        help="Output directory",
+    )
+    return parser.parse_args()
+
+
 def create_multi_class_labels(labels: np.ndarray, max_label: int) -> np.ndarray:
+    """
+
+    TODO: Docs
+    """
     multi_class_labels = np.zeros_like(labels)
     for i, label in enumerate(labels):
         if label > max_label:
@@ -18,46 +50,70 @@ def create_multi_class_labels(labels: np.ndarray, max_label: int) -> np.ndarray:
     return multi_class_labels
 
 
-# Load data
-print("Preparing data...")
-word_meaning_train_data = pd.read_csv("data/word_meaning_train_data.csv")
-word_meaning_data_cols = word_meaning_train_data.columns.values
-word_meaning_data_feature_cols = np.array(
-    [col for col in word_meaning_data_cols if col.startswith("X_")]
-)
+def estimate_num_meanings_supervised(train_data_filepath: str, output_dir: str):
+    """
+    TODO: Docs
 
-# Split into X and y
-X_train = minmax_scale(word_meaning_train_data[word_meaning_data_feature_cols].values)
-y_train = word_meaning_train_data["y"].values
+    Parameters
+    ----------
+    train_data_filepath : str
+        Filepath of word meaning training data.
+    output_dir : str
+        Output directory.
+    """
 
-# Create multi-class labels
-max_y_multi = np.quantile(y_train, q=0.9)
-print(f"Max label: {max_y_multi}")
-y_train_multi_class = create_multi_class_labels(labels=y_train, max_label=max_y_multi)
-print("Done!")
+    # Load data
+    print("Preparing data...")
+    word_meaning_train_data = pd.read_csv(train_data_filepath)
+    word_meaning_data_cols = word_meaning_train_data.columns.values
+    word_meaning_data_feature_cols = np.array(
+        [col for col in word_meaning_data_cols if col.startswith("X_")]
+    )
 
-# Parameters
-cv = 20
-max_iter = 1000000
-l1_alphas = np.linspace(0.00000001, 0.1, 10000)
-print("Running LogisticRegressionCV...")
-log_reg_cv = LogisticRegressionCV(
-    Cs=1 / l1_alphas,
-    cv=cv,
-    max_iter=max_iter,
-    penalty="l1",
-    solver="saga",
-    n_jobs=-1,
-    random_state=rng_seed,
-    verbose=0,
-)
-log_reg_cv.fit(X_train, y_train_multi_class)
-print("Done!")
+    # Split into X and y
+    X_train = minmax_scale(
+        word_meaning_train_data[word_meaning_data_feature_cols].values
+    )
+    y_train = word_meaning_train_data["y"].values
 
-print("Saving to file...")
-joblib.dump(log_reg_cv, "data/log_reg_cv.joblib", protocol=4)
-print("Done!")
+    # Create multi-class labels
+    max_y_multi = np.quantile(y_train, q=0.9)
+    print(f"Max label: {max_y_multi}")
+    y_train_multi_class = create_multi_class_labels(
+        labels=y_train, max_label=max_y_multi
+    )
+    print("Done!")
 
-print(f"L1 ratios: {log_reg_cv.l1_ratio_}")
+    # Parameters
+    cv = 20
+    max_iter = 1000000
+    l1_alphas = np.linspace(0.00000001, 0.1, 10000)
+    print("Running LogisticRegressionCV...")
+    log_reg_cv = LogisticRegressionCV(
+        Cs=1 / l1_alphas,
+        cv=cv,
+        max_iter=max_iter,
+        penalty="l1",
+        solver="saga",
+        n_jobs=-1,
+        random_state=rng_seed,
+        verbose=0,
+    )
+    log_reg_cv.fit(X_train, y_train_multi_class)
+    print("Done!")
 
-# TODO: Add argparse to script and generalize
+    print("Saving to file...")
+    joblib.dump(log_reg_cv, join(output_dir, "log_reg_cv.joblib"), protocol=4)
+    print("Done!")
+
+    print(f"L1 ratios: {log_reg_cv.l1_ratio_}")
+
+    # TODO: Add argparse to script and generalize
+
+
+if __name__ == "__main__":
+    args = parse_args()
+    estimate_num_meanings_supervised(
+        train_data_filepath=args.train_data_filepath,
+        output_dir=args.output_dir,
+    )
--- code/analysis_of_embeddings/estimate_num_meanings_supervised.sh
+++ code/analysis_of_embeddings/estimate_num_meanings_supervised.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
-screen -dmS estimate_num_meanings_supervised -L -Logfile estimate_num_meanings_supervised.logs python estimate_num_meanings_supervised.py
+screen -dmS estimate_num_meanings_supervised -L -Logfile estimate_num_meanings_supervised.logs python estimate_num_meanings_supervised.py \
+--train_data_filepath data/word_meaning_train_data.csv \
+--output_dir data
 screen -r estimate_num_meanings_supervised
